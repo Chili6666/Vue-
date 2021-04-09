836247417e1ce9e87aef65f1916f21e6167f92ca
Volker Esser <volker@esser-work.de>
2021-04-09 19:55:13 +0200

    login with overlay

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,17 +2,13 @@
   <div class="login-box">
     <h4>Login</h4>
     <hr />
-    {{ isloading }}
-    <form class="form-group" @submit.prevent="login">
-      <div class="input-field">
-        <label>Username</label>
-        <input required v-model="username" placeholder="Name" />
-      </div>
-      <div>
-        <label>Movie Db Token</label>
-        <input required v-model="requestToken" placeholder="Token" />
-      </div>
-      <hr />
+    <div v-if="isloading" class="container-loading">
+      <Spinner />
+    </div>
+
+    <form @submit.prevent="login">
+      <input required v-model="username" placeholder="Name" />
+      <input required v-model="requestToken" placeholder="Token" />
       <button type="submit">Login</button>
     </form>
   </div>
@@ -25,8 +21,12 @@ import { useUserStore } from "@/stores/userStore";
 import { useRouter } from "vue-router";
 import { IUser } from "@/models/user";
 import AuthService from "@/services/AuthService";
+import Spinner from "@/components/spinner.vue";
 
 export default {
+  components: {
+    Spinner,
+  },
   setup() {
     const store = useUserStore();
     const router = useRouter();
@@ -74,25 +74,44 @@ h4 {
 }
 
 .login-box {
-  width: 400px;
-  height: auto;
-  background-color: white;
+  border: 1px solid black;
   border-radius: 5px;
-  padding: 40px;
-  margin: auto;
-  margin-top: 60px;
-  border: 1px solid #e4e6e7;
-  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.4);
-  text-align: left;
+  padding: 1.5rem;
+  width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  overflow: hidden;
+}
+
+.container-loading {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.3);
 }
 
 button {
-  margin: auto;
-  background-color: #fa3254;
-  margin: 0;
-  padding: 0px 40px;
+  margin: 10px 0px;
+  padding: 0.5rem;
+  background-color: lightgray;
+  border: 1px solid gray;
+  border-radius: 3px;
+  cursor: pointer;
 }
-button i {
-  font-size: 18px;
+
+input {
+  padding: 0.5rem;
+    margin: 4px 0px;
+}
+
+form {
+  display: flex;
+  flex-flow: column;
 }
 </style>
